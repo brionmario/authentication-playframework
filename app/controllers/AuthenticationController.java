6ec885471b14e0f8ab1f5c5fe75aad240cbb241b
Brion Mario <brion.2015283@iit.ac.lk>
2017-03-15 03:00:22 +0530

    Fixed a small bug

--- app/controllers/AuthenticationController.java
+++ app/controllers/AuthenticationController.java
@@ -33,33 +33,59 @@ public class AuthenticationController extends Controller {
 
     }
 
+    /**
+     * Renders the login page when the route file calls this method
+     * @return Returns the login.scala.html page
+     */
     public Result index() {
 
         return ok(views.html.login.render());
     }
 
+    /**
+     * Renders the signup page when the route file calls this method
+     * @return Returns the signup.scala.html page
+     */
     public Result signup() {
 
         return ok(views.html.signup.render());
     }
 
-    public Result homePage(String fullname) {
 
-        return ok(views.html.index.render(fullname));
+    /**
+     * Renders the home page when the route file calls this method
+     * @param fullName Full name ( First Name & Last Name ) of the user is passed
+     *                 from the getUser() method  via this parameter
+     * @return Returns the signup.scala.html page and the name of the user logged in
+     */
+    public Result homePage(String fullName) {
+
+        return ok(views.html.index.render(fullName));
 
     }
 
+    /**
+     * Method to add a new user to the databse
+     * @return Returns a success message
+     */
     public Result addUser() {
 
         DynamicForm requestData = formFactory.form().bindFromRequest();
 
         //add the values posted by the form and create a new user.
         //P.S : data has to be passed according to the order of parameters taken by the model constructor.
-        userDB.add(new User(requestData.get("firstName"), requestData.get("lastName"), requestData.get("email"), requestData.get("password")));
+        userDB.add(new User(requestData.get("firstName"), requestData.get("lastName"),
+                requestData.get("email"), requestData.get("password")));
+
         return ok("User Added Successfully");
     }
 
 
+    /**
+     * This method checks if the user already has an account and if he/she does , It will redirect the user to the
+     * Home page
+     * @return If the user is found Returns the home page, and if not will display an error.
+     */
     public Result getUser() {
 
         DynamicForm requestData = formFactory.form().bindFromRequest();
@@ -73,22 +99,24 @@ public class AuthenticationController extends Controller {
         String lName = null;
         String fullName = null;
 
-        User user = null;
-        boolean flag = false;
+        User loggeUuser = null;
+        boolean isUserFound = false;
 
-        for (User u : userDB) {
-            if (u.getEmail().equals(email) && u.getPassword().equals(password)) {
-                user = u;
-                fName = u.getFirstName();
-                lName = u.getLastName();
+        //iterating through the user database
+        for (User user : userDB) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                loggeUuser = user;
+                fName = user.getFirstName();
+                lName = user.getLastName();
                 fullName = fName + " " + lName;
-                flag = true;
+                isUserFound = true;
             }
         }
 
-        if (flag) {
+        if (isUserFound) {
+            
             return redirect(routes.AuthenticationController.homePage(fullName));
-            //return ok(user.getName() + " is logged in with ID " + user.getMailID());
+
         }
         return ok("No such user password combination found");
     }
